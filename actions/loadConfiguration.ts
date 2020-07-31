010ea5264f7215c2dc607468cfe87553e5fa2ef9
duart38 <mr.codercuracao@gmail.com>
2020-07-31 20:43:58 +0200

    Added performance mark for loadConfiguration

--- actions/loadConfiguration.ts
+++ actions/loadConfiguration.ts
@@ -3,6 +3,8 @@ import { getUrlParams } from "./decoding.ts";
 import { modelWatcher } from "../server.ts";
 import { Connection } from "../enums/connectionTypes.ts";
 import { HTTPModelMethod, SOCKETModelMethod } from "../interfaces/model.ts";
+import { print } from "./logging.ts";
+import { Verbosity } from "../enums/verbosity.ts";
 
 export async function loadConfiguration(
   model: string,
@@ -10,9 +12,14 @@ export async function loadConfiguration(
   req: any,
   connectionType: Connection,
 ): Promise<HTTPModelMethod | SOCKETModelMethod> {
+  let fileHash = modelWatcher.getObservable().getValue();
+  performance.mark("start_"+fileHash)
   let m = await import(
-    `../${modelsFolder()}/${model}?${modelWatcher.getObservable().getValue()}.ts`
+    `../${modelsFolder()}/${model}?${fileHash}.ts`
   );
+  performance.mark("end_"+fileHash)
+  let time = performance.measure("config_load", `start_${fileHash}`, `end_${fileHash}`).duration
+  print(`[!] configuration loaded in: ${time} ms`, Verbosity.HIGH)
   if (typeof m[Object.keys(m)[0]] == "function") {
     var temp = await m[Object.keys(m)[0]](req.headers, getUrlParams(req.url)); // headers and url params are available in models
     return temp[connectionType][method];
