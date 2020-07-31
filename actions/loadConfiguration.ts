8a43906491b0c139aae62dadde93660a49e0342c
duart38 <mr.codercuracao@gmail.com>
2020-07-31 11:59:45 +0200

    Added boilerplate method to load internal configuration

---
+++ actions/loadConfiguration.ts
@@ -0,0 +1,7 @@
+/**
+ * Calls the database and loads expected behavior for our components
+ * E.g: server will instruct websocket what to do when 'test' message is emitted from the client
+ */
+export function loadConfiguration(){
+    return {};
+}
