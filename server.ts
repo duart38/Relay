255bee55c29713d7cf336fa9d3cda3bb81bfef4b
duart38 <mr.codercuracao@gmail.com>
2020-07-31 12:28:48 +0200

    Added boilerplate all acepting server

--- server.ts
+++ server.ts
@@ -1,6 +1,11 @@
-const dbMap = {
-    getAllProducts: {
-        route: "/call/to/ISS/server",
-        // any other config here.. (future proofing)
-    }
+import { serve, Server } from "https://deno.land/std/http/server.ts";
+import { constructHeaders } from "./actions/respond.ts";
+import config from "./config.js";
+
+const port: number = config.port;
+const server = serve({ port });
+console.log(`[+] Server running on port: ${port}`);
+
+for await (const req of server) {
+  req.respond({ body: "hello world", headers: constructHeaders(req) });
 }
