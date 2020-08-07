230c35c0778fdcb4817edfecd4241c8524b39f78
duart38 <mr.codercuracao@gmail.com>
2020-08-07 21:42:46 +0200

    Fixed an issue with content length being passed down

--- actions/respond.ts
+++ actions/respond.ts
@@ -1,7 +1,8 @@
 import { HTTPModelMethod } from "../interfaces/model.ts";
 
-export function constructHeaders(req: any, HTTPModelMethod: HTTPModelMethod) {
-  let headers = req.headers;
+export function constructHeaders(req: any, HTTPModelMethod: HTTPModelMethod): Headers {
+  let headers = <Headers>req.headers;
+  headers.delete("Content-Length")
 
     Object.entries(HTTPModelMethod.cors).map((val)=>{
       headers.append(val[0], val[1])
