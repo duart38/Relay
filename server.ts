3657430806ca1d7b59c7a4a1afa86223edf59ade
duart38 <mr.codercuracao@gmail.com>
2020-08-07 16:34:00 +0200

    Removed unused websocket component

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
 import { modelsFolder } from "./CLA.ts";
 import httpServer from "./components/httpServer.ts";
-import SocketBridge from "./components/websocket.ts";
+//import SocketBridge from "./components/websocket.ts";
 import { Watcher } from "./components/fileWatcher.ts";
 
 export const modelWatcher = new Watcher(modelsFolder()); // watching the base models folder
 
 new httpServer();
-new SocketBridge(6969);
+//new SocketBridge(6969);
