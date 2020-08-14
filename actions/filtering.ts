5a5f59eebbf696ac291c9ec1d415844f7b0cf2d3
duart38 <mr.codercuracao@gmail.com>
2020-08-14 13:18:29 +0200

    Added logging for header check

--- actions/filtering.ts
+++ actions/filtering.ts
@@ -1,8 +1,11 @@
 import { HTTPModelMethod } from "../interfaces/model.ts";
+import { Verbosity } from "../enums/verbosity.ts";
+import { print } from "./logging.ts";
 
 export function checkRequiredHeaders(config: HTTPModelMethod, headers: Headers): boolean{
     if(config.requiredHeaders){
         let t = config.requiredHeaders?.map((val)=>headers.has(val));
+        print(t.includes(false) ? "[-] One or more required headers not passed. Sending error" : "[+] Header check passed", Verbosity.MEDIUM)
         return !t.includes(false)
     }
     return true;
